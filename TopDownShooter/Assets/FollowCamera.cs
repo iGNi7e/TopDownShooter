deb72f11b77f3bb4b95292f421dacabc32e70074
iGNiTe <ignarmezh@gmail.com>
2018-04-08 18:17:32 +0300

    Create first version player controller

---
+++ TopDownShooter/Assets/FollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+    public Transform target;
+
+    private Vector3 currentVelocity;
+
+    private void LateUpdate()
+    {
+        if (target == null)
+            return;
+        Vector3 newPos = new Vector3(target.transform.position.x,transform.position.y,target.transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position,newPos,ref currentVelocity,0.1f);
+        //transform.rotation = Quaternion.Euler(new Vector3(0f,0f,0f));
+    }
+}
---
+++ TopDownShooter/Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private Quaternion targetRotation;
+
+    public float rotationSpeed = 450f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 9f;
+
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+
+        if(input != Vector3.zero)
+        {
+            targetRotation = Quaternion.LookRotation(input);
+            transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y,rotationSpeed * Time.deltaTime);
+        }
+
+        Vector3 motion = input;
+        motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1) ? .7f : 1;
+        motion *= (Input.GetButton("Run")) ? runSpeed : walkSpeed;
+        motion += Vector3.up * -8f;
+        controller.Move(motion * Time.deltaTime);
+    }
+}
