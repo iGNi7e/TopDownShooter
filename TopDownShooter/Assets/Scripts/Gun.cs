d5db25f5bf28e380c81c198e7e45735b13ba2f8e
iGNiTe <ignarmezh@gmail.com>
2018-04-12 01:48:43 +0300

    Create shoots

---
+++ TopDownShooter/Assets/Scripts/Gun.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Gun : MonoBehaviour
+{
+
+    public enum GunType { Semi, Burst, Auto }
+    public GunType gunType;
+    public float rpm;
+
+    //Components
+    public Transform spawn;
+    public AudioClip clip;
+    private LineRenderer tracer;
+
+    //System
+    private float secondsBetweenShots;
+    private float nextPossibleShootTime;
+
+    private void Start()
+    {
+        secondsBetweenShots = 60 / rpm;
+        if (GetComponent<LineRenderer>())
+        {
+            tracer = GetComponent<LineRenderer>();
+        }
+    }
+
+    public void Shoot()
+    {
+
+        if (CanShoot())
+        {
+            Ray ray = new Ray(spawn.position,spawn.forward);
+            RaycastHit hit;
+
+            float distanceShot = 20f;
+
+            if (Physics.Raycast(ray,out hit,distanceShot))
+            {
+                distanceShot = hit.distance;
+            }
+            //Debug.DrawLine(ray.origin,ray.direction * distanceShot,Color.red,1);
+            nextPossibleShootTime = Time.time * secondsBetweenShots;
+
+            GetComponent<AudioSource>().PlayOneShot(clip);
+            if (tracer) StartCoroutine("RendreTracer",ray.direction * distanceShot);
+        }
+    }
+
+    IEnumerator RendreTracer(Vector3 hitPoint)
+    {
+        tracer.enabled = true;
+        tracer.SetPosition(0,spawn.position);
+        tracer.SetPosition(1,spawn.position + hitPoint);
+        yield return null;
+        tracer.enabled = false;
+    }
+
+    public void ShootContinue()
+    {
+        if (gunType == GunType.Auto)
+        {
+            Shoot();
+        }
+    }
+
+    private bool CanShoot()
+    {
+        bool canShoot = true;
+
+        if (Time.time < nextPossibleShootTime)
+        {
+            canShoot = false;
+        }
+
+        return canShoot;
+    }
+
+}
---
+++ TopDownShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private Quaternion targetRotation;
+
+    public float rotationSpeed = 450f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 9f;
+
+    public Gun gun;
+    private Camera cam;
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        //Movement();
+        ControlMouse();
+        if (Input.GetButtonDown("Shoot"))
+        {
+            gun.Shoot();
+        }
+        else if(Input.GetButton("Shoot"))
+        {
+            gun.ShootContinue();
+        }
+    }
+
+    void ControlMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = cam.ScreenToWorldPoint(new Vector3(mousePos.x,mousePos.y,cam.transform.position.y-transform.position.y));
+        targetRotation = Quaternion.LookRotation(mousePos -  new Vector3(transform.position.x,0,transform.position.z));
+        transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y,rotationSpeed * Time.deltaTime);
+
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+
+        Vector3 motion = input;
+        motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1) ? .7f : 1;
+        motion *= (Input.GetButton("Run")) ? runSpeed : walkSpeed;
+        motion += Vector3.up * -8f;
+        controller.Move(motion * Time.deltaTime);
+    }
+
+
+    void Movement()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+
+        if (input != Vector3.zero)
+        {
+            targetRotation = Quaternion.LookRotation(input);
+            transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y,rotationSpeed * Time.deltaTime);
+        }
+
+        Vector3 motion = input;
+        motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1) ? .7f : 1;
+        motion *= (Input.GetButton("Run")) ? runSpeed : walkSpeed;
+        motion += Vector3.up * -8f;
+        controller.Move(motion * Time.deltaTime);
+    }
+}
